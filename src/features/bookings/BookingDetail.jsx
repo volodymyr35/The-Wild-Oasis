51333370d09c733329f370a0fd78ccfb726dc35a
volodymyr35 <vzhukevych@gmail.com>
2024-02-27 20:37:39 +0200

    Initial commit

---
+++ src/features/bookings/BookingDetail.jsx
@@ -0,0 +1,101 @@
+import { useNavigate } from "react-router-dom";
+import { useBooking } from "./useBooking";
+import { useMoveBack } from "../../hooks/useMoveBack";
+import { HiArrowUpOnSquare } from "react-icons/hi2";
+import { useCheckout } from "../check-in-out/useCheckout";
+import styled from "styled-components";
+
+import BookingDataBox from "./BookingDataBox";
+import Row from "../../ui/Row";
+import Heading from "../../ui/Heading";
+import Tag from "../../ui/Tag";
+import ButtonGroup from "../../ui/ButtonGroup";
+import Button from "../../ui/Button";
+import Modal from "../../ui/Modal";
+import ConfirmDelete from "../../ui/ConfirmDelete";
+
+import ButtonText from "../../ui/ButtonText";
+import Spinner from "../../ui/Spinner";
+import { useDeleteBooking } from "./useDeleteBooking";
+import Empty from '../../ui/Empty';
+
+const HeadingGroup = styled.div`
+  display: flex;
+  gap: 2.4rem;
+  align-items: center;
+`;
+
+function BookingDetail() {
+  const { booking, isLoading } = useBooking();
+  const { checkout, isCheckingOut } = useCheckout();
+  const { deleteBooking, isDeleting } = useDeleteBooking();
+
+  const moveBack = useMoveBack();
+  const navigate = useNavigate();
+
+  if (isLoading) return <Spinner />;
+  if (!booking) return <Empty resourceName='booking' />;
+
+  const statusToTagName = {
+    unconfirmed: "blue",
+    "checked-in": "green",
+    "checked-out": "silver",
+  };
+
+  const { id: bookingId, status = "" } = booking || {};
+
+  return (
+    <>
+      <Row type="horizontal">
+        <HeadingGroup>
+          <Heading type="h1">Booking #{bookingId}</Heading>
+          <Tag type={statusToTagName[status]}>{status.replace("-", " ")}</Tag>
+        </HeadingGroup>
+        <ButtonText onClick={moveBack}>&larr; Back</ButtonText>
+      </Row>
+
+      <BookingDataBox booking={booking} />
+
+      <ButtonGroup>
+        {status === "unconfirmed" && (
+          <Button onClick={() => navigate(`/checkin/${bookingId}`)}>
+            Check in
+          </Button>
+        )}
+
+        {status === "checked-in" && (
+          <Button
+            icon={<HiArrowUpOnSquare />}
+            onClick={() => checkout(bookingId)}
+            disabled={isCheckingOut}
+          >
+            Check out
+          </Button>
+        )}
+
+        <Modal>
+          <Modal.Open opens="delete">
+            <Button variation="danger">Delete booking</Button>
+          </Modal.Open>
+          <Modal.Window name="delete">
+            <ConfirmDelete
+              resourceName="booking"
+              disabled={isDeleting}
+              onConfirm={() =>
+                deleteBooking(bookingId, {
+                  onSettled: () => navigate(-1),
+                })
+              }
+            />
+          </Modal.Window>
+        </Modal>
+
+        <Button variation="secondary" onClick={moveBack}>
+          Back
+        </Button>
+      </ButtonGroup>
+    </>
+  );
+}
+
+export default BookingDetail;
---
+++ src/services/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
+
+export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+
+export default createClient(supabaseUrl, supabaseKey);
